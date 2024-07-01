32cf52146b210cd205c7dd2cb4c0ab6bc5ab3e55
Jorge Celaya <jcelaya775@gmail.com>
2024-07-01 14:31:43 -0500

    update

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { RsdoctorRspackPlugin } from '@rsdoctor/rspack-plugin';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  server: {
+    publicDir: {
+      // name: 'src/static',
+    },
+  },
+  plugins: [
+    pluginReact(),
+    pluginSvgr({
+      svgrOptions: {
+        exportType: 'named',
+      },
+    }),
+  ],
   dev: {
     liveReload: true,
+  },
+  tools: {
+    rspack(config, { appendPlugins }) {
+      appendPlugins(new RsdoctorRspackPlugin({}));
+    },
   },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css';
+import { ReactComponent as Logo } from './vector.svg'; // TODO: configure public path (CRA should support this out of the box)
+import Picture from '/public/images/audience.jpg';
+import Jazz from './jazz.jpeg';
+// TODO: Is there a way to configure /public alias?
 
 const App = () => {
   console.log(process.env.PUBLIC_MODE);
+  console.log(Picture);
+  console.log(Jazz);
 
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
       <h2>MODE: {process.env.PUBLIC_MODE}</h2>
       <p>Start building amazing things with Rsbuild.</p>
+      <p>ASSET_PREFIX: {process.env.ASSET_PREFIX}</p>
+      <p>SVG:</p>
+      <Logo className="logo" />
+      <p>IMG:</p>
+      <img src="/images/audience.jpg" alt="audience" />
     </div>
   );
 };
